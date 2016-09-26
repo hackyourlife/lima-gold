c42f330590c2e9136a47ede8140f43cd5181b205
thomasba <git@thomasba.de>
2016-09-26 21:54:47 +0200

    Moved morse stuff to extra file

--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,5 @@
 pkgname=("lima-gold")
-pkgver=1.1.21
+pkgver=1.1.22
 pkgrel=1
 pkgdesc="A terminal based Jabber MUC client with support for encrypted and invisible messages"
 arch=("any")
@@ -7,16 +7,17 @@ url="https://github.com/hackyourlife/lima-gold"
 license=("Custom")
 depends=("python" "python-sleekxmpp" "python-crypto" "words")
 optdepends=("espeak" "python-pyaudio")
-source=(client.py encryptim.py main.py api.py rl.py rp.py rot.py espeak.py setup.py)
+source=(client.py encryptim.py main.py api.py rl.py rp.py rot.py espeak.py setup.py morse.py)
 md5sums=('d9e638aceae3e0724deb2691bd6f2847'
          '851a9f27429ca1dd6792cde39b061d2d'
-         '8ef6733285bb9a9d29e60e2fc757b7ee'
+         'eeaef0fea1e14a14307786adf6ec4892'
          '34d41be6e5bd7128a80163f869372b12'
          '5e1c0e2735ee8750ee2f29e1a4c3eefa'
          '34553800636e433a5a95aa69e64734be'
          '9335387c75f65281e7846f90053376fe'
          '7a073c923ef9760c52f05a134625fb0f'
-         'be06c215cf34d5af64287366bcbc48de')
+         'be06c215cf34d5af64287366bcbc48de'
+         '01d4450f3f840555ccafd7b389a571b7')
 
 package() {
   cd "$srcdir"
---
+++ morse.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# vim:set ts=8 sts=8 sw=8 tw=80 noet cc=80:
+"""
+Functions to encode and decode morse code
+"""
+
+
+CHARS_TO_MORSE = {"A": "·−", "B": "−···", "C": "−·−·", "D": "−··", "E": "·",
+		"F": "··−·", "G": "−−·", "H": "····", "I": "··", "J": "·−−−",
+		"K": "−·−", "L": "·−··", "M": "−−", "N": "−·", "O": "−−−",
+		"P": "·−−·", "Q": "−−·−", "R": "·−·", "S": "···", "T": "−",
+		"U": "··−", "V": "···−", "W": "·−−", "X": "−··−", "Y": "−·−−",
+		"Z": "−−··", "1": "·−−−−", "2": "··−−−", "3": "···−−",
+		"4": "····−", "5": "·····", "6": "−····", "7": "−−···",
+		"8": "−−−··", "9": "−−−−·", "0": "−−−−−", "À": "·−−·−",
+		"Ä": "·−·−", "È": "·−··−", "É": "··−··", "Ö": "−−−·",
+		"Ü": "··−−", "ß": "···−−··", "CH": "−−−−", "Ñ": "−−·−−",
+		".": "·−·−·−", ",": "−−··−−", ":": "−−−···", ";": "−·−·−·",
+		"?": "··−−··", "-": "−····−", "_": "··−−·−", "(": "−·−−·",
+		")": "−·−−·−", "'": "·−−−−·", "=": "−···−", "+": "·−·−·",
+		"/": "−··−·", "@": "·−−·−·", "!": "−·−·−−", '"': "·−··−·",
+		"$": "···−··−"}
+
+MORSE_TO_CHARS = {CHARS_TO_MORSE[key]: key for key in CHARS_TO_MORSE}
+
+def encode(msg):
+	"""
+	Decode a string to morse code
+	"""
+	text = []
+	for letter in msg.upper():
+		if letter in CHARS_TO_MORSE:
+			text.append(CHARS_TO_MORSE[letter])
+		elif letter == " ":
+			text.append("")
+		else:
+			text.append(CHARS_TO_MORSE["?"])
+	return " ".join(text)
+
+def decode(msg):
+	"""
+	Decode a string containing morse code
+	"""
+	tmp = []
+	for letter in msg.replace(".", "·").replace("-", "−").split(" "):
+		if len(letter) == 0:
+			tmp.append(" ")
+		elif letter in MORSE_TO_CHARS:
+			tmp.append(MORSE_TO_CHARS[letter])
+	return "".join(tmp).lower()
